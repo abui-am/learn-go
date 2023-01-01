357c73ec24b8bf8e04a1aab182ca97d136e0bc32
Abui <adjiem31@gmail.com>
2023-01-01 23:12:19 +0700

    git commit 14 5

---
+++ 14/5.go
@@ -0,0 +1,60 @@
+package main
+
+import "fmt"
+
+const NMAX = 5
+
+type Employee struct {
+	id          int
+	firstName   string
+	lastName    string
+	phoneNumber int
+	email       string
+	division    string
+	branch      string
+}
+
+type Employees [NMAX]Employee
+
+func main() {
+	var employees Employees
+	var editIndex int
+	fillArray(&employees)
+
+	fmt.Scan(&editIndex)
+	editData(editIndex, &employees)
+	sortDataByFirstName(&employees)
+	fmt.Print("hello world")
+}
+
+func fillArray(employees *Employees) {
+	for i := 0; i < NMAX; i++ {
+		e := &employees[i]
+		fmt.Scan(&e.id, &e.firstName, &e.lastName, &e.phoneNumber, &e.email, &e.division, &e.branch)
+	}
+
+}
+
+func editData(i int, employees *Employees) {
+
+	if i >= NMAX || i < 0 {
+		fmt.Print("Data tidak ditemukan")
+	} else {
+		var e = &employees[i]
+		// *ID must not be editable
+		fmt.Scan(&e.firstName, &e.lastName, &e.phoneNumber, &e.email, &e.division, &e.branch)
+	}
+}
+
+func sortDataByFirstName(t *Employees) {
+	for i := 0; i < NMAX; i++ {
+		minIndex := i
+		for j := i + 1; j < NMAX; j++ {
+			if t[j].firstName < t[minIndex].firstName {
+				minIndex = j
+			}
+		}
+		// Swap the minimum element with the current element
+		t[i], t[minIndex] = t[minIndex], t[i]
+	}
+}
