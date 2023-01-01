7e74786926c5ef4faf38f6cd03dd8b69edc531da
Abui <adjiem31@gmail.com>
2023-01-01 23:06:33 +0700

    git commit 14 4

--- 14/4.go
+++ 14/4.go
@@ -20,13 +20,27 @@ func main() {
 	var employees Employees
 
 	fillArray(&employees)
+	editData(&employees)
 	fmt.Print("hello world")
-
 }
 
 func fillArray(employees *Employees) {
 	for i := 0; i < NMAX; i++ {
 		e := &employees[i]
 		fmt.Scan(&e.id, &e.firstName, &e.lastName, &e.phoneNumber, &e.email, &e.division, &e.branch)
+	}
+
+}
+
+func editData(employees *Employees) {
+	var i int
+	fmt.Scan(&i)
+
+	if i >= NMAX || i < 0 {
+		fmt.Print("Data tidak ditemukan")
+	} else {
+		var e = &employees[i]
+		// *ID must not be editable
+		fmt.Scan(&e.firstName, &e.lastName, &e.phoneNumber, &e.email, &e.division, &e.branch)
 	}
 }
