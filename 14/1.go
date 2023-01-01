c8f734cb1504b9b51f88fa5228415fea571a1686
Abui <adjiem31@gmail.com>
2023-01-01 22:22:26 +0700

    git commit 14 1

---
+++ 14/1.go
@@ -0,0 +1,11 @@
+package main
+
+type Employee struct {
+	id          int
+	firstName   string
+	lastName    string
+	phoneNumber int
+	email       string
+	division    string
+	branch      string
+}
